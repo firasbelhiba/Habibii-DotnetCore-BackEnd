3c6c93167e98d3c10b14673b58890ef8eb6ad96c
firasbelhiba <firas.belhiba@esprit.tn>
2021-01-11 18:27:39 +0100

    Register Validation (annotation)

--- Habibii/Dtos/UserForRegisterDto.cs
+++ Habibii/Dtos/UserForRegisterDto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,8 +8,11 @@ namespace Habibii.Dtos
 {
     public class UserForRegisterDto
     {
+        [Required]
         public string Username { get; set; }
 
+        [Required]
+        [StringLength(8, MinimumLength = 4 , ErrorMessage = "You must specify password between 4 and 8 characters")]
         public string Password { get; set; }
     }
 }
