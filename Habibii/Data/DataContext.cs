60ca54bb26a0b9fd56b57fd086c92bf790ebbd11
firasbelhiba <firas.belhiba@esprit.tn>
2021-01-11 01:04:49 +0100

    First Commit

---
+++ Habibii/Data/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using Habibii.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Habibii.Data
+{
+    public class DataContext : DbContext 
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base (options){}
+
+        public DbSet<Value> Values { get; set; }
+        public DbSet<User> Users { get; set; }
+    }
+}
